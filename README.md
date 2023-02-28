1f2ca3e057535537f6609ca645c6c9d83019b01a
bedroesb <bedro@psb.vib-ugent.be>
2023-02-28 10:48:46 +0100

    new theme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 Pages are rendered using Jekyll based on the markdown files in the `master` branch.
 These changes get rendered to HTML pages and pushed to the `gh-pages` branch from where it is deployed using GitHub Pages.
+
+
+To change the url in the welcome page of usegalaxy.be, change: /srv/galaxy/shared/src/static/welcome.html
---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+---
+layout: none
+permalink: assets/js/main.js
+---
+
+/**
+ * Activate tooltips
+ */
+
+$(function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl, {
+            animation: false
+        })
+    })
+})
+
+/**
+ * Making tables responsive
+ */
+$(document).ready(function () {
+    $('table').each(function (i, obj) {
+        if (!$(obj).parents('.table-responsive').length) {
+            $(obj).wrap('<div class="table-responsive mt-4 mb-5"/>');
+        }
+    });
+
+});
